6d53a64fad29820a1d43f8e54d141d0fc38283e8
Nicolás Jara <nicolas.jarachm@gmail.comm>
2026-02-11 17:05:40 -0300

    add endpoint to retrieve payment information

--- app/main.py
+++ app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "API funcionando 🚀"}
+
+@app.get("/payments")
+def get_payments():
+    return [
+        {"id": 1, "amount": 100, "status": "completed"},
+        {"id": 2, "amount": 250, "status": "pending"}
+    ]
+
