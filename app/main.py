c649717b6ab4743fc4e5db0e84b5987f1cc7cc99
Nicolás Jara <nicolas.jarachm@gmail.comm>
2026-02-13 15:29:04 -0300

    refactor: restructure transaction handling and endpoints in main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Transaction(BaseModel): # Definimos un modelo de datos para las transacciones
+    user: str
+    amount: float
+    status: str
 
 app = FastAPI()
 
-@app.get("/")
+transactions = []
+
+@app.get("/") #creamos un endpoint para la raíz de la API
 def home():
     return {"message": "API funcionando 🚀"}
 
-@app.get("/payments")
-def get_payments():
-    return [
-        {"id": 1, "amount": 100, "status": "completed"},
-        {"id": 2, "amount": 250, "status": "pending"}
-    ]
+@app.post("/transactions") #creamos un endpoint para crear transacciones
+def create_transaction(transaction: Transaction):
+    transactions.append(transaction) # Guardamos la transacción en memoria (en una lista)
+    return {
+        "message": "Transacción creada",
+        "data": transaction
+    }
 
+@app.get("/transactions") #creamos un endpoint para obtener todas las transacciones
+def get_transactions():
+    return transactions
